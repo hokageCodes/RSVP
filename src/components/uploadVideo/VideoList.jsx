0c176c1c6dc676e3209bd893dacc78401509a21f
hokageCodes <hashsenju94@gmail.com>
2023-04-08 05:30:55 +0100

    working

--- src/components/uploadVideo/VideoList.jsx
+++ src/components/uploadVideo/VideoList.jsx
@@ -1,127 +1,135 @@
-import { useState, useEffect } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import './uploadform.css'
+import { useState, useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "./uploadform.css";
+import _ from "lodash";
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 6;
 
 const VideoList = () => {
   const [videos, setVideos] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [lastDocument, setLastDocument] = useState(null);
   const [totalPages, setTotalPages] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [likedVideos, setLikedVideos] = useState([]);
+
 
   useEffect(() => {
     const fetchVideos = async () => {
       setIsLoading(true);
       try {
-        const firestore = firebase.firestore();
-        const videosRef = firestore.collection('videos').orderBy('name').limit(PAGE_SIZE);
-        const videosSnapshot = await videosRef.get();
-        const videosData = videosSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        setVideos(videosData);
-        setLastDocument(videosSnapshot.docs[videosSnapshot.docs.length - 1]);
-        setTotalPages(Math.ceil(videosSnapshot.size / PAGE_SIZE));
-        setIsLoading(false);
+        const videosSnapshot = await firebase
+          .firestore()
+          .collection("videos")
+          .get();
+        const videos = videosSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setVideos(videos);
       } catch (error) {
         setError(error);
-        setIsLoading(false);
       }
+      setIsLoading(false);
     };
     fetchVideos();
   }, []);
 
   useEffect(() => {
-    const fetchVideos = async () => {
-      setIsLoading(true);
-      try {
-        const firestore = firebase.firestore();
-        const videosRef = firestore.collection('videos').orderBy('name').startAfter(lastDocument).limit(PAGE_SIZE);
-        const videosSnapshot = await videosRef.get();
-        const videosData = videosSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        setVideos((prevVideos) => [...prevVideos, ...videosData]);
-        setLastDocument(videosSnapshot.docs[videosSnapshot.docs.length - 1]);
-        setIsLoading(false);
-      } catch (error) {
-        setError(error);
-        setIsLoading(false);
-      }
-    };
-    if (currentPage > 1) {
-      fetchVideos();
-    }
-  }, [currentPage]);
+    const numPages = Math.ceil(videos.length / PAGE_SIZE);
+    setTotalPages(numPages);
+  }, [videos]);
 
-  const handleLikeClick = async (id) => {
-    try {
-      const firestore = firebase.firestore();
-      const videoRef = firestore.collection('videos').doc(id);
-      const videoSnapshot = await videoRef.get();
-      const videoData = videoSnapshot.data();
-      const updatedLikes = videoData.likes + 1;
-      await videoRef.update({ likes: updatedLikes });
+  const handleLikeClick = (id) => {
+    if (likedVideos.includes(id)) {
+      // If video is already liked, remove it from the array and decrease likes by 1
+      setLikedVideos((prevLikedVideos) => prevLikedVideos.filter((likedId) => likedId !== id));
       setVideos((prevVideos) =>
-        prevVideos.map((video) => (video.id === id ? { ...video, likes: updatedLikes } : video))
+        prevVideos.map((video) => {
+          if (video.id === id) {
+            return {
+              ...video,
+              likes: video.likes - 1,
+            };
+          }
+          return video;
+        })
       );
-    } catch (error) {
-      setError(error);
+    } else {
+      // If video is not liked, add it to the array and increase likes by 1
+      setLikedVideos((prevLikedVideos) => [...prevLikedVideos, id]);
+      setVideos((prevVideos) =>
+        prevVideos.map((video) => {
+          if (video.id === id) {
+            return {
+              ...video,
+              likes: video.likes + 1,
+            };
+          }
+          return video;
+        })
+      );
     }
   };
-
-  const handlePrevPageClick = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
+  
+  
+  
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
   };
 
-  const handleNextPageClick = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
-
-  const paginatedVideos = videos.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
+  const paginatedVideos = _.chunk(videos, PAGE_SIZE)[currentPage - 1] || [];
 
   return (
     <div className="video-list">
       {isLoading ? (
-        <div className="video-list__loading">Loading...</div>
+        <div className="video-list-loading">Loading...</div>
       ) : error ? (
-        <div className="video-list__error">{error.message}</div>
+        <div className="video-list-error">{error.message}</div>
       ) : (
         <>
           {paginatedVideos.map((video) => (
-            <div key={video.id} className="video-list__item">
-            <video src={video.url} controls className="video-list__video"></video>
-            <div className="video-list__details">
-              <h2 className          ="video-list__name">{video.name}</h2>
-          <button onClick={() => handleLikeClick(video.id)} className="video-list__like">
-            {video.likes} Likes
-          </button>
-        </div>
-      </div>
-      ))}
-      <div className="video-list__pagination">
-        <button
-          onClick={handlePrevPageClick}
-          disabled={currentPage === 1}
-          className="video-list__pagination-btn"
-        >
-          Prev
-        </button>
-        <span className="video-list__pagination-info">
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          onClick={handleNextPageClick}
-          disabled={currentPage === totalPages}
-          className="video-list__pagination-btn"
-        >
-          Next
-        </button>
-      </div>
-    </>
-  )}
-</div>
-);
+            <div key={video.id} className="video-list-item">
+              <video src={video.url} controls className="video-list-video"></video>
+              <div className="video-list-details">
+                <h2 className="video-list-name">{video.name}</h2>
+                <button
+                  onClick={() => handleLikeClick(video.id)}
+                  className="video-list-like"
+                  disabled={likedVideos.includes(video.id)}
+                >
+                  {video.likes} Likes
+                </button>
+
+              </div>
+            </div>
+          ))}
+          <div className="video-list-pagination">
+            {currentPage > 1 && (
+              <button
+                onClick={() => handlePageChange(currentPage - 1)}
+                className="video-list-pagination-btn"
+              >
+                Prev
+              </button>
+            )}
+            <span className="video-list-pagination-info">
+              Page {currentPage} of {totalPages}
+            </span>
+            {currentPage < totalPages && (
+              <button
+                onClick={() => handlePageChange(currentPage + 1)}
+                className="video-list-pagination-btn"
+              >
+                Next
+              </button>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default VideoList;
--- src/components/uploadVideo/index.jsx
+++ src/components/uploadVideo/index.jsx
@@ -2,119 +2,128 @@ import { useState } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
 import 'firebase/compat/firestore';
+import './uploadform.css';
 
 const UploadForm = () => {
-  const [name, setName] = useState('');
-  const [videoFile, setVideoFile] = useState(null);
-  const [uploadProgress, setUploadProgress] = useState(0);
-  const [uploadError, setUploadError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
+const [name, setName] = useState('');
+const [videoFile, setVideoFile] = useState(null);
+const [uploadProgress, setUploadProgress] = useState(0);
+const [uploadError, setUploadError] = useState(null);
+const [successMessage, setSuccessMessage] = useState(null);
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setVideoFile(file);
-    }
-  };
-
-  const handleUpload = async (e) => {
-    e.preventDefault();
-    setUploadProgress(0);
-
-    try {
-      if (!videoFile) {
-        setUploadError('Please select a video to upload');
-        return;
-      }
-
-      const storageRef = firebase.storage().ref();
-      const videoRef = storageRef.child(videoFile.name);
-      const uploadTask = videoRef.put(videoFile);
-
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setUploadProgress(progress);
-        },
-        (error) => {
-          setUploadError(error.message);
-        },
-        async () => {
-          const downloadUrl = await videoRef.getDownloadURL();
-          setSuccessMessage('Video uploaded successfully!');
-
-          const firestore = firebase.firestore();
-          await firestore.collection('videos').add({
-            name,
-            url: downloadUrl,
-            likes: 0,
-          });
-
-          setName('');
-          setVideoFile(null);
-        }
-      );
-    } catch (error) {
-      setUploadError(error.message);
-    }
-  };
-
-  return (
-    <div className="upload-form">
-      <h1 className="upload-form__title">Video Uploader</h1>
-      <form onSubmit={handleUpload} noValidate>
-        <div className="upload-form__field">
-          <label className="upload-form__label" htmlFor="name">
-            Name:
-          </label>
-          <input
-            id="name"
-            type="text"
-            placeholder="Enter video name"
-            value={name}
-            maxLength="50"
-            required
-            onChange={handleNameChange}
-            className="upload-form__input"
-          />
-        </div>
-        <div className="upload-form__field">
-          <label className="upload-form__label" htmlFor="video">
-            Video:
-          </label>
-          <input
-            id="video"
-            type="file"
-            accept="video/*"
-            required
-            onChange={handleFileChange}
-            className="upload-form__input"
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={!videoFile}
-          className="upload-form__button"
-        >
-          Upload
-        </button>
-      </form>
-      {uploadProgress > 0 && (
-        <div className="upload-form__progress">
-          Uploading... {Math.round(uploadProgress)}%
-        </div>
-      )}
-      {uploadError && <div className="upload-form__error">{uploadError}</div>}
-      {successMessage && (
-        <div className="upload-form__success">{successMessage}</div>
-      )}
-    </div>
-  );
+const handleNameChange = (e) => {
+setName(e.target.value);
 };
 
-export default UploadForm;
+const handleFileChange = (e) => {
+const file = e.target.files[0];
+if (file) {
+setVideoFile(file);
+}
+};
+
+const handleUpload = async (e) => {
+e.preventDefault();
+setUploadProgress(0);
+setUploadError(null);
+setSuccessMessage(null);
+
+try {
+  if (!videoFile) {
+    setUploadError('Please select a video to upload');
+    return;
+  }
+
+  const storageRef = firebase.storage().ref();
+  const videoRef = storageRef.child(videoFile.name);
+  const uploadTask = videoRef.put(videoFile);
+
+  uploadTask.on(
+    'state_changed',
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setUploadProgress(progress);
+    },
+    (error) => {
+      setUploadError(error.message);
+    },
+    async () => {
+      try {
+        const downloadUrl = await videoRef.getDownloadURL();
+        setSuccessMessage('Video uploaded successfully!');
+
+        const firestore = firebase.firestore();
+        await firestore.collection('videos').add({
+          name,
+          url: downloadUrl,
+          likes: 0,
+          comments: [],
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+
+        setName('');
+        setVideoFile(null);
+      } catch (error) {
+        setUploadError(error.message);
+      }
+    }
+  );
+} catch (error) {
+  setUploadError(error.message);
+}
+}
+return (
+  <div className="UploadForm">
+  <h1 className="UploadForm__title">Upload Your Video</h1>
+  <form onSubmit={handleUpload} noValidate>
+  <div className="UploadForm__field">
+  <label className="UploadForm__label" htmlFor="name">
+  Video Name:
+  </label>
+  <input
+           id="name"
+           type="text"
+           placeholder="Enter video name"
+           value={name}
+           maxLength="50"
+           required
+           onChange={handleNameChange}
+           className="UploadForm__input"
+         />
+  </div>
+  <div className="UploadForm__field">
+  <label className="UploadForm__label" htmlFor="video">
+  Select a video file:
+  </label>
+  <input
+           id="video"
+           type="file"
+           accept="video/*"
+           required
+           onChange={handleFileChange}
+           className="UploadForm__input"
+         />
+  </div>
+  <button
+         type="submit"
+         disabled={!videoFile}
+         className="UploadForm__button"
+       >
+  Upload
+  </button>
+  </form>
+  {uploadProgress > 0 && (
+  <div className="UploadForm__progress">
+  Uploading... {Math.round(uploadProgress)}%
+  </div>
+  )}
+  {uploadError && <div className="UploadForm__error">{uploadError}</div>}
+  {successMessage && (
+  <div className="UploadForm__success">{successMessage}</div>
+  )}
+  </div>
+)} 
+
+
+export default UploadForm
+  
